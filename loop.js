af8e5a56e6c2b9d8563b533333004757885737b1
justinemhars123 <113317566+justinemhars123@users.noreply.github.com>
2022-11-07 08:32:33 +0800

    Add files via upload

---
+++ loop.js
@@ -0,0 +1,20 @@
+var num = window.prompt("Number:")
+
+var num1 = parseInt(num);
+
+console.log('Number: ' + num1);
+
+if (num1%2 == 0){
+    console.log('even');
+}
+else {
+    console.log('odd');
+}
+
+
+console.log('LOOP');
+
+for (let i= 1; i<=5; i++) {
+  console.log(i);
+}
+
